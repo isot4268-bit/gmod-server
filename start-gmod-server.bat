010f2c67dc77530bc450998626e6f3190822223c
isot4268-bit <isot4268@gmail.com>
2026-07-01 01:20:49 +0000

    Allow GMod local HTTP sync calls

--- start-gmod-server.bat
+++ start-gmod-server.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0server"
-srcds.exe -console -game garrysmod +gamemode sandbox +map gm_construct +maxplayers 16 -port 27015
+srcds.exe -console -game garrysmod -allowlocalhttp +gamemode sandbox +map gm_construct +maxplayers 16 -port 27015
